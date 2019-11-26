584749bb7652d8841641c21e0cf184901dab56dc
AndyMcElligott <andymc2315@gmail.com>
2019-11-26 10:00:47 -0600

    initial commit

--- server/server.js
+++ server/server.js
@@ -7,11 +7,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 const houses = require('./routes/house.router');
 
 // Serve back static files by default
 app.use(express.static('server/public'));
-
+app.use('/house', houses);
 
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
