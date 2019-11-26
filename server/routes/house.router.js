d6e927990be3a9e750423ba990db5f03686a7173
AndyMcElligott <andymc2315@gmail.com>
2019-11-26 10:22:01 -0600

    POST created in house.router.js

--- server/routes/house.router.js
+++ server/routes/house.router.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+router.post('/', (req, res)=>{
+    let queryString = `INSERT INTO "listings" ("cost", "sqft", "type", "city", "image_path")
+    VALUES ($1, $2, $3, $4, $5);`;
+    let house = req.body;
+    pool.query(queryString, [house.cost, house.sqft, house.type, house.city, house.image_path])
+    .then(result =>{
+        console.log('in router /POST, Added house:', result);
+        res.sendStatus(201);
+    }).catch(error =>{
+        console.log(error);
+        res.sendStatus(400);
+    });
+}); // end POST
+
 
 
 module.exports = router;
